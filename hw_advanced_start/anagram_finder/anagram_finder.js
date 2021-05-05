f056494721e51ece48b96cc06b88fd791c66d95a
Allen_K <allenkelly278@gmail.com>
2021-05-05 21:07:06 +0100

    add function comments

--- hw_advanced_start/anagram_finder/anagram_finder.js
+++ hw_advanced_start/anagram_finder/anagram_finder.js
@@ -6,15 +6,15 @@ const AnagramFinder = function (word) {
 
 AnagramFinder.prototype.findAnagrams = function(otherWords) {
     const anagrams = otherWords.filter((word) => {
-        return this.sortWord(word) === this.lower_sorted && this.word !== word.toLowerCase();
+        // 1: compare sorted words                         2: check for duplicate words 
+        return (this.sortWord(word) === this.lower_sorted) && (this.word !== word.toLowerCase());
     });
     return anagrams;
 };
 
 AnagramFinder.prototype.sortWord = function(word) {
-    // sorting words to reveal anagrams
-    // make the word lowercase, split into a character array, sort it (ascending or descending), 
-    // then join the letters again to make a character-sorted word. 
+    // make the word lowercase, split into a character array, sort it (ascending or descending doesnt matter), 
+    // then join the letters again to make a sorted string. 
     return word.toLowerCase().split("").sort().join("");
 }
 
